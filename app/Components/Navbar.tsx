8972e5926089c2a7039535269996a1c0c722a539
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2024-07-23 14:33:36 +0000

    Navbar Perfected

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -7,11 +7,11 @@ const Navbar = () => {
       <div className="w-full max-w-screen-xl flex items-center justify-between px-4">
         <div className="flex items-center gap-4">
           <Image src="/portfolio_logo.svg" alt="logo" width={40} height={40} />
-          <p className="hidden sm:block text-white">Peacefill Tawiah</p>
+          <p className="hidden sm:block text-white font-Poppins text-[16px] font-normal font-weight-500">Peacefill Tawiah</p>
         </div>
         <button className="w-40 h-11 rounded-full bg-gradient-to-r from-[#8A74FF] via-[#1A1632] to-[#8A74FF] p-[1px]">
           <div className="flex items-center justify-center h-full w-full bg-black rounded-full">
-            <span className="text-white">Get In Touch</span>
+            <span className="text-white font-Poppins text-[15px] font-normal">Get In Touch</span>
           </div>
         </button>
       </div>
