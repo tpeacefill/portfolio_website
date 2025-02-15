7b40e25d1a2094f5f1e46af59d7aa33a45bd9553
Peacefill Amenuveve Tawiah <pamenuveve.ac@st.vvu.edu.gh>
2025-02-15 11:01:47 +0000

    Modified work experience , giving it more detail

--- app/Sections/WorkExperience.tsx
+++ app/Sections/WorkExperience.tsx
@@ -67,9 +67,64 @@ const WorkExperience: React.FC = () => {
           >
             2020
           </m.h1>
-          <div className="relative flex flex-col items-center w-full">
-            <div className="w-0.5 h-[400px] bg-white bg-opacity-30"></div>
+          
+          {/* Vertical Road Timeline Container */}
+          <div className="relative flex flex-col items-center w-full h-[400px]">
+            {/* Vertical Road SVG */}
+            <m.svg 
+              width="100" 
+              height="400" 
+              viewBox="0 0 100 400" 
+              className="absolute"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.8 }}
+            >
+              {/* Road Outer Edge (shadow) */}
+              <m.path
+                d="M 50,0 C 70,100 30,200 50,300 C 70,400 50,400 50,400"
+                fill="none"
+                stroke="rgba(0,0,0,0.5)"
+                strokeWidth="24"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+              />
+              {/* Road Base */}
+              <m.path
+                d="M 50,0 C 70,100 30,200 50,300 C 70,400 50,400 50,400"
+                fill="none"
+                stroke="#2A2A42"
+                strokeWidth="20"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+              />
+              {/* Road Edges */}
+              <m.path
+                d="M 50,0 C 70,100 30,200 50,300 C 70,400 50,400 50,400"
+                fill="none"
+                stroke="#8a74ff66"
+                strokeWidth="22"
+                strokeDasharray="2,20"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.6, ease: "easeInOut" }}
+              />
+              {/* Center Line */}
+              <m.path
+                d="M 50,0 C 70,100 30,200 50,300 C 70,400 50,400 50,400"
+                fill="none"
+                stroke="#8a74ff"
+                strokeWidth="2"
+                strokeDasharray="10,10"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.8, ease: "easeInOut" }}
+              />
+            </m.svg>
 
+            {/* Timeline Points */}
             <div className="absolute flex flex-col items-center w-full h-[400px] justify-around">
               <div className="relative flex flex-col items-center">
                 <div className="relative w-[26px] h-[26px] bg-[#8a74ff] rounded-full flex items-center justify-center">
@@ -120,23 +175,78 @@ const WorkExperience: React.FC = () => {
           >
             2021
           </m.h1>
-          <div className="relative flex flex-col items-center justify-center w-full lg:h-0.5">
-            <div className="w-full h-0.5 bg-white bg-opacity-30"></div>
+          
+          {/* Horizontal Road Timeline Container */}
+          <div className="relative flex flex-col items-center justify-center w-full h-32">
+            {/* Horizontal Road SVG */}
+            <m.svg 
+              width="100%" 
+              height="100%" 
+              viewBox="0 0 1000 128" 
+              className="absolute"
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{ duration: 0.8 }}
+            >
+              {/* Road Outer Edge (shadow) */}
+              <m.path
+                d="M 100,64 C 250,64 300,32 500,64 C 700,96 750,64 900,64"
+                fill="none"
+                stroke="rgba(0,0,0,0.5)"
+                strokeWidth="24"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+              />
+              {/* Road Base */}
+              <m.path
+                d="M 100,64 C 250,64 300,32 500,64 C 700,96 750,64 900,64"
+                fill="none"
+                stroke="#2A2A42"
+                strokeWidth="20"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.5, ease: "easeInOut" }}
+              />
+              {/* Road Edges */}
+              <m.path
+                d="M 100,64 C 250,64 300,32 500,64 C 700,96 750,64 900,64"
+                fill="none"
+                stroke="#8a74ff66"
+                strokeWidth="22"
+                strokeDasharray="2,20"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.6, ease: "easeInOut" }}
+              />
+              {/* Center Line */}
+              <m.path
+                d="M 100,64 C 250,64 300,32 500,64 C 700,96 750,64 900,64"
+                fill="none"
+                stroke="#8a74ff"
+                strokeWidth="2"
+                strokeDasharray="10,10"
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 1.8, ease: "easeInOut" }}
+              />
+            </m.svg>
 
+            {/* Timeline Points */}
             <m.div 
-              className="absolute w-[26px] h-[26px] bg-[#8a74ff] rounded-full lg:left-[20%] flex items-center justify-center lg:-translate-x-1/2"
+              className="absolute w-[26px] h-[26px] bg-[#8a74ff] rounded-full left-[20%] flex items-center justify-center -translate-x-1/2"
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.3 }}
             >
               <m.div 
-                className="absolute w-0.5 h-[70px] bg-[#8a74ff] lg:bottom-[-70px]"
+                className="absolute w-0.5 h-[70px] bg-[#8a74ff] bottom-[-70px]"
                 initial={{ scaleY: 0 }}
                 whileInView={{ scaleY: 1 }}
                 transition={{ duration: 0.3, delay: 0.5 }}
               />
               <m.p 
-                className="text-white text-opacity-50 text-[20px] font-Poppins font-thin lg:absolute lg:top-[100px] -ml-0 whitespace-nowrap"
+                className="text-white text-opacity-50 text-[20px] font-Poppins font-thin absolute top-[100px] -ml-0 whitespace-nowrap"
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 0.6 }}
@@ -146,19 +256,19 @@ const WorkExperience: React.FC = () => {
             </m.div>
 
             <m.div 
-              className="absolute w-[26px] h-[26px] bg-[#8a74ff] rounded-full lg:left-[50%] flex items-center justify-center lg:-translate-x-1/2"
+              className="absolute w-[26px] h-[26px] bg-[#8a74ff] rounded-full left-[50%] flex items-center justify-center -translate-x-1/2"
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.6 }}
             >
               <m.div 
-                className="absolute w-0.5 h-[70px] bg-[#8a74ff] lg:top-[-70px]"
+                className="absolute w-0.5 h-[70px] bg-[#8a74ff] top-[-70px]"
                 initial={{ scaleY: 0 }}
                 whileInView={{ scaleY: 1 }}
                 transition={{ duration: 0.3, delay: 0.8 }}
               />
               <m.p 
-                className="text-white text-opacity-50 text-[20px] font-Poppins font-thin lg:absolute lg:bottom-[100px] -ml-0 whitespace-nowrap"
+                className="text-white text-opacity-50 text-[20px] font-Poppins font-thin absolute bottom-[100px] -ml-0 whitespace-nowrap"
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 0.9 }}
@@ -168,19 +278,19 @@ const WorkExperience: React.FC = () => {
             </m.div>
 
             <m.div 
-              className="absolute w-[26px] h-[26px] bg-[#8a74ff] rounded-full lg:left-[80%] flex items-center justify-center lg:-translate-x-1/2"
+              className="absolute w-[26px] h-[26px] bg-[#8a74ff] rounded-full left-[80%] flex items-center justify-center -translate-x-1/2"
               initial={{ scale: 0, opacity: 0 }}
               whileInView={{ scale: 1, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.9 }}
             >
               <m.div 
-                className="absolute w-0.5 h-[70px] bg-[#8a74ff] lg:bottom-[-70px]"
+                className="absolute w-0.5 h-[70px] bg-[#8a74ff] bottom-[-70px]"
                 initial={{ scaleY: 0 }}
                 whileInView={{ scaleY: 1 }}
                 transition={{ duration: 0.3, delay: 1.1 }}
               />
               <m.p 
-                className="text-white text-opacity-50 text-[20px] font-Poppins font-thin lg:absolute lg:top-[100px] -ml-1 whitespace-nowrap"
+                className="text-white text-opacity-50 text-[20px] font-Poppins font-thin absolute top-[100px] -ml-1 whitespace-nowrap"
                 initial={{ opacity: 0, y: 20 }}
                 whileInView={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.3, delay: 1.2 }}
